efc41e491304dcd25ca6ab3dd125ed1302035fed
Diego Alencar Jacober <diegoalencar.jacober@gmail.com>
2023-11-09 08:08:16 -0300

    Configurando docker

--- dev.sh
+++ dev.sh
@@ -1,1 +1,1 @@
-uvicorn port.fastapi.application:core_module --proxy-headers --host 0.0.0.0 --port 3000 --reload
+uvicorn port.fastapi.application:core_module --proxy-headers --host 0.0.0.0 --port 8000 --reload
